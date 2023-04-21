009dfe4325d3d1843b5e58b4c6a5c164638d100e
viro <twikista@gmail.com>
2023-04-21 18:15:13 +0100

    completed exercises 3.1-3.6

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((i) => i.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "Person not in phonebook";
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  persons = persons.filter((i) => i.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+  const alreadyExist = persons.find((i) => i.name === name);
+
+  if (!name) {
+    return response.status(400).json({ error: "name is required" });
+  }
+
+  if (!number) {
+    return response.status(400).json({ error: "number is required" });
+  }
+
+  if (alreadyExist) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
+
+  const person = { id: Math.floor(Math.random() * 100), name, number };
+  persons = persons.concat(person);
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const requestTime = new Date();
+  const numberOfPersons = persons.length;
+  response.send(`
+  <p>phonebook has infor for ${numberOfPersons}</p>
+  <p>${requestTime}</p>`);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
