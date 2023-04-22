602c7ba9ab44d58c6d071e87e9e3c1b7dfe3ed7f
viro <twikista@gmail.com>
2023-04-22 04:27:09 +0100

    completed exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+//morgan tokens
+morgan.token("data", (req) => JSON.stringify(req.body));
+
 app.use(express.json());
+app.use(
+  morgan(`:method :url :status :res[content-length] - :response-time ms :data`)
+);
 
 let persons = [
   {
